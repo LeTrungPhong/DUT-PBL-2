5e6e31d814c8ecf31c1252f9a580aca66cf6021b
LeTrungPhong <letrungphong22122004@gmail.com>
2023-11-14 04:41:35 +0700

    start

---
+++ quanly.cpp
@@ -0,0 +1,82 @@
+#include "quanly.h"
+
+QuanLy::QuanLy()
+{ }
+
+QuanLy::~QuanLy()
+{
+    delete[] this->tt;
+}
+
+void QuanLy::NhapDuLieuQuanLy(string tk, string mk, int size, TiepTan *t)
+{
+    this->TaiKhoan = tk;
+    this->MatKhau = mk;
+    this->size = size;
+    this->tt = new TiepTan[this->size];
+    for(int i = 0; i < this->size; i++)
+    {
+        *(this->tt + i) = *(t + i);
+    }
+}
+
+void QuanLy::NhapThongTinChucNang(const QuanLyKhachHang &k, const QuanLyPhong &p)
+{
+    this->k = k;
+    this->p = p;
+}
+
+const QuanLy& QuanLy::operator = (const QuanLy& q)
+{
+    if(this != &q)
+    {
+        this->CCCD = q.CCCD;
+        this->SDT = q.SDT;
+        this->Birth.Ngay = q.Birth.Ngay;
+        this->Birth.Thang = q.Birth.Thang;
+        this->Birth.Nam = q.Birth.Nam;
+        this->TaiKhoan = q.TaiKhoan;
+        this->MatKhau = q.MatKhau;
+        this->size = q.size;
+        this->tt = new TiepTan[this->size];
+        for(int i = 0; i < this->size; i++)
+        {
+            *(this->tt + i) = *(q.tt + i);
+        }
+    }
+    return *this;
+}
+
+bool QuanLy::KiemTraTiepTanDangNhap(string tk, string mk)
+{
+    for(int i = 0; i < this->size; i++)
+    {
+        if((this->tt + i)->KiemTraTaiKhoanMatKhau(tk,mk))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+TiepTan& QuanLy::LayThongTinTiepTan(string tk, string mk)
+{
+    for(int i = 0; i < this->size; i++)
+    {
+        if((this->tt + i)->KiemTraTaiKhoanMatKhau(tk,mk))
+        {
+            return *(this->tt + i);
+        }
+    }
+    TiepTan temp;
+    return temp;
+}
+
+bool QuanLy::KiemTraQuanLyDangNhap(string tk, string mk)
+{
+    if(this->TaiKhoan == tk && this->MatKhau == mk)
+    {
+        return true;
+    }
+    return false;
+}
